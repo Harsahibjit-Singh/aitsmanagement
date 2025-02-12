5b207c43115baa24b8e78a53c303687aa2a53a76
Harsahibjit Singh <129825567+Harsahibjit-Singh@users.noreply.github.com>
2025-02-12 23:10:15 +0530

    Update AdminNotification.tsx

--- aitsmanage/src/components/AdminNotification.tsx
+++ aitsmanage/src/components/AdminNotification.tsx
@@ -10,7 +10,7 @@ const AdminNotification = () => {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/api/messages', {
+      const response = await axios.post('https://aitsmanagement.onrender.com', {
         name,
         rank,
         message,
